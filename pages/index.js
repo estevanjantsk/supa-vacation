ad702b2a4fc4e3b8119f074608420481b2bb6e1d
Estevan Jantsk <estevan.jantsk@gmail.com>
2022-03-02 12:27:35 +0200

    Query data in Next.js with Server-Side Rendering (SSR)

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,20 @@
 import Layout from '@/components/Layout';
 import Grid from '@/components/Grid';
 
-import homes from 'data.json';
+import { prisma, serialize } from "../prisma";
 
-export default function Home() {
+export async function getServerSideProps() {
+  // Get all homes
+  const homes = await prisma.home.findMany();
+  // Pass the data to the Home page
+  return {
+    props: {
+      homes: serialize(homes),
+    }
+  };
+}
+
+export default function Home({ homes = [] }) {
   return (
     <Layout>
       <h1 className="text-xl font-medium text-gray-800">
---
+++ prisma/index.js
@@ -0,0 +1,14 @@
+// Import the generated Prisma client
+import { PrismaClient } from '@prisma/client'
+
+// Instantiate it
+const prisma = new PrismaClient()
+
+const serialize = (data) => {
+	return JSON.parse(JSON.stringify(data));
+}
+
+export {
+	prisma,
+	serialize
+}
