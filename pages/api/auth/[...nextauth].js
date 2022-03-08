b296acd0aae29e34ab9e580a93db63864ab32a6d
Estevan Jantsk <estevan.jantsk@gmail.com>
2022-03-08 09:11:42 +0200

    customiza sign-in email

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -2,8 +2,40 @@ import NextAuth from 'next-auth';
 import { PrismaAdapter } from '@next-auth/prisma-adapter';
 import EmailProvider from 'next-auth/providers/email';
 import nodemailer from 'nodemailer';
+import Handlebars from 'handlebars';
+import { readFileSync } from 'fs';
+import path from 'path';
 
 import { prisma } from "../../../prisma";
+
+const emailsDir = path.resolve(process.cwd(), 'emails');
+
+const sendVerificationRequest = ({ identifier, url }) => {
+  const emailFile = readFileSync(path.join(emailsDir, 'confirm-email.html'), {
+    encoding: 'utf8',
+  });
+  const emailTemplate = Handlebars.compile(emailFile);
+  transporter.sendMail({
+    from: `"✨ SupaVacation" ${process.env.EMAIL_FROM}`,
+    to: identifier,
+    subject: 'Your sign-in link for SupaVacation',
+    html: emailTemplate({
+      base_url: process.env.NEXTAUTH_URL,
+      signin_url: url,
+      email: identifier,
+    }),
+  });
+};
+
+const transporter = nodemailer.createTransport({
+  host: process.env.EMAIL_SERVER_HOST,
+  port: process.env.EMAIL_SERVER_PORT,
+  auth: {
+    user: process.env.EMAIL_SERVER_USER,
+    pass: process.env.EMAIL_SERVER_PASSWORD,
+  },
+  secure: true,
+});
 
 export default NextAuth({
   pages: {
@@ -14,15 +46,7 @@ export default NextAuth({
   },
   providers: [
     EmailProvider({
-      server: {
-        host: process.env.EMAIL_SERVER_HOST,
-        port: process.env.EMAIL_SERVER_PORT,
-        auth: {
-          user: process.env.EMAIL_SERVER_USER,
-          pass: process.env.EMAIL_SERVER_PASSWORD,
-        },
-      },
-      from: process.env.EMAIL_FROM,
+      sendVerificationRequest,
       maxAge: 10 * 60, // Magic links are valid for 10 min only
     }),
   ],
