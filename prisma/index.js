9083366798ee3d43507a5e64ab7cce91b5cc067f
Estevan Jantsk <estevan.jantsk@gmail.com>
2022-05-13 11:24:27 +0300

    global instances

--- prisma/index.js
+++ prisma/index.js
@@ -3,16 +3,24 @@ import { PrismaClient } from '@prisma/client'
 import { createClient } from '@supabase/supabase-js'
 
 // Instantiate it
-const prisma = new PrismaClient()
+const prisma = global.prisma || new PrismaClient();
+
+if (process.env.NODE_ENV !== 'production') {
+  global.prisma = prisma;
+}
+
+const supabase = global.supabase || createClient(
+  process.env.SUPABASE_URL,
+  process.env.SUPABASE_KEY
+);
+
+if (process.env.NODE_ENV !== 'production') {
+  global.supabase = supabase;
+}
 
 const serialize = (data) => {
   return JSON.parse(JSON.stringify(data));
 }
-
-const supabase = createClient(
-  process.env.SUPABASE_URL,
-  process.env.SUPABASE_KEY
-);
 
 export {
   prisma,
